f9eb0543fee38c5f0850d48cbc7b982ac5ddc94c
Anuragsomwanshi <anuragsomwanshi11@gmail.com>
2024-10-16 19:44:46 +0530

    next version of project

--- Index.js
+++ Index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8000;
 const person = require('./database');
+
 app.get('/',(req,res)=>{
 
   console.log("hello laturkar");
@@ -18,3 +19,6 @@ app.use('/person',personData);
 app.listen(port,()=>{
   console.log("server started at port:",port);
 })
+
+
+// commit added
